e3f36e98043983390cd305bc21611a92f425aa7c
Jonathan Escamilla <admin@Jonathans-MacBook-Pro.local>
2020-09-20 16:16:14 -0700

    finish set up of mikro-orm on file

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ import microConfig from './mikro-orm.config';
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
+
+  // example of how to create a post with mikro-orm
   // const post = orm.em.create(Post, {title: 'my first post'});
   // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  // example of how to fetch for all posts with mikro-orm
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
-
 
 main().catch((err) => {
   console.error(err);
