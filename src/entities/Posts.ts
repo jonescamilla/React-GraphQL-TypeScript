ead89b93bfebc36b8d7641408afb2adf2b7c127d
Jonathan Escamilla <admin@Jonathans-MacBook-Pro.local>
2020-09-23 14:11:15 -0700

    add @Field decorators for type-graphql

--- src/entities/Posts.ts
+++ src/entities/Posts.ts
@@ -1,17 +1,26 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType} from "type-graphql";
 
+// decorator of ObjectType to convert classes to graphql type with graphql-type
+@ObjectType()
+// entity decorator
 @Entity()
 export class Post {
+  // Field decorator exposes to graphql schema
   // the post will have an id
+  @Field()
   @PrimaryKey()
   id!: number;
   // there will be a date on creation
+  @Field(() => String)
   @Property({ type: 'date'})
   createdAt = new Date();
   // date on update
+  @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
   // && title
+  @Field()
   @Property({type: 'text'})
   title!: string;
 }
