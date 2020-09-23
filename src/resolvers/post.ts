2d8ca37ef7108f7539356e7d1bbc1db5f6571b57
Jonathan Escamilla <admin@Jonathans-MacBook-Pro.local>
2020-09-23 14:45:13 -0700

    new type-graphql query for search/post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,12 +1,20 @@
-import { Post } from 'src/entities/Posts';
+import { Post } from '../entities/Posts';
 import { MyContext } from 'src/types';
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Int } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
-  // to set the type for type-graphql you 
+  // to set the type for type-graphql of an array you wrap the type in square brackets
   @Query(() => [Post])
   posts(@Ctx() {em}: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+  // to set the type or potentially null type-graphql has an object with options
+  @Query(() => Post, {nullable: true})
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() {em}: MyContext
+    ): Promise<Post | null> {
+    return em.findOne(Post, {id});
   }
 }
