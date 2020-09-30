a7d6031857824883d32d8573d825ddff8c7ea430
jon.escamilla <jonathanescamilla1@gmail.com>
2020-09-29 18:52:12 -0700

    implemnetation of redis, session, connectRedis in index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,36 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
 const main = async () => {
   // connection to the database
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   // initiate express server
   const app = express();
+  // redis implementation
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+  // run the session middleware before the apollo middleware because it will be used inside apollo
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        // look into this setting
+        sameSite: "lax",
+        secure: __prod__, // cookie only works in https
+      },
+      secret: "aaroisetnarosietnaorisetnaorisent",
+      resave: false,
+    })
+  );
   // create a new instance of ApolloServer
   const apolloServer = new ApolloServer({
     // passing in our schema
@@ -24,7 +48,7 @@ const main = async () => {
       validate: false,
     }),
     // context is an object that is accessible by all the resolvers
-    context: () => ({ em: orm.em }),
+    context: ({req, res}): MyContext => ({ em: orm.em, req, res }),
   });
   // apply apollo middleware
   apolloServer.applyMiddleware({ app });
