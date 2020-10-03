fbe679cf234059042f09db1fe396c2a630ad6677
jon.escamilla <jonathanescamilla1@gmail.com>
2020-10-02 17:48:38 -0700

    cache updates for both login and register in _app.tsx

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -35,7 +35,7 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
     );
   }
   return (
-    <Flex bg="tomato" p={4} ml={"auto"}>
+    <Flex bg="tan" p={4} ml={"auto"}>
       <Box ml={"auto"}></Box>
       {body}
     </Flex>
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,63 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import React from "react";
-import { createClient, Provider } from "urql";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
+import theme from "../theme";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
 
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
+
+// urql client
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, _args, cache, _info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) return query;
+                else return { me: result.login.user };
+              }
+            );
+          },
+          register: (_result, _args, cache, _info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) return query;
+                else return { me: result.register.user };
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
-
-import theme from "../theme";
 
 function MyApp({ Component, pageProps }: any) {
   return (
