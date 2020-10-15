8c7249f68709831df2dafe6c8256136ea94c1038
jon.escamilla <jonathanescamilla1@gmail.com>
2020-10-15 15:53:16 -0700

    organize imports and basic index logic for creating a post link

--- web/src/pages/create-post.tsx
+++ web/src/pages/create-post.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Input } from '@chakra-ui/core';
+import { Box, Button } from '@chakra-ui/core';
 import { Form, Formik } from 'formik';
 import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
 import InputField from '../components/InputField';
 import Layout from '../components/Layout';
-import { useCreatePostMutation, useMeQuery } from '../generated/graphql';
+import { useCreatePostMutation } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { useIsAuth } from '../utils/useIsAuth';
 
