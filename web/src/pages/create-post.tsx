a541eab0a0b05ea7ac8011c3d4213a907d58b55e
jon.escamilla <jonathanescamilla1@gmail.com>
2020-10-15 15:28:37 -0700

    create-post page w/ error handling and form

---
+++ web/src/pages/create-post.tsx
@@ -0,0 +1,55 @@
+import { Box, Button, Input } from '@chakra-ui/core';
+import { Form, Formik } from 'formik';
+import { withUrqlClient } from 'next-urql';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import InputField from '../components/InputField';
+import Layout from '../components/Layout';
+import { useCreatePostMutation, useMeQuery } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+
+interface CreatePostProps {}
+
+const CreatePost: React.FC<CreatePostProps> = ({}) => {
+  // const [{ data, fetching }] = useMeQuery();
+  const router = useRouter();
+  // useEffect(() => {
+  //   if (!fetching && !data?.me) router.replace('/login');
+  // }, [data, router]);
+  const [, createPost] = useCreatePostMutation();
+  return (
+    <Layout variant="small">
+      <Formik
+        initialValues={{ title: '', text: '' }}
+        onSubmit={async (values) => {
+          const { error } = await createPost({ input: values });
+          if (!error) router.push('/');
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" placeholder="title" label="Title" />
+            <Box mt={4}>
+              <InputField
+                name="text"
+                placeholder="text..."
+                label="Body"
+                textarea
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              variantColor="teal"
+            >
+              create post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
+};
+// import urql client in order to do mutations
+export default withUrqlClient(createUrqlClient)(CreatePost);
