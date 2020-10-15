296e133007ea30e2cd4b04cf82410e28b69faf77
jon.escamilla <jonathanescamilla1@gmail.com>
2020-10-15 11:22:30 -0700

    remove migrations folder from server and remove mikro-orm.config

--- server/src/migrations/Migration20200925215205.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20200925215205 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('alter table "user" drop constraint "user_password_unique";');
-  }
-
-}
